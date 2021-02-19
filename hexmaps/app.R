a2eff20396f8deee888fb018d5b1d1b13386ef56
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-02-19 11:07:37 +0000

    maps

--- hexmaps/app.R
+++ hexmaps/app.R
@@ -1,11 +1,21 @@
 library(shiny)
+library(r2d3)
 
 ui <- fluidPage(
-  
+  inputPanel(
+    sliderInput("bar_max", label = "Max:",
+                min = 0, max = 1, value = 1, step = 0.05)
+  ),
+  textOutput("d3")
 )
 
-server <- function(input, output, session) {
-  
+server <- function(input, output) {
+  output$d3 <- renderD3({
+    r2d3(
+      "/Users/fgu/dev/projects/learning/hexmaps/nuts3.hexjson",
+      script = file.path("/Users/fgu/dev/projects/learning/hexmaps/map.js")
+    )
+  })
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
